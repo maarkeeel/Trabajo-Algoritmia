d91c4427f0d48394ad3f0baae51beb965c9dcfa4
FerminEzcurdia <149517760+FerminEzcurdia@users.noreply.github.com>
2023-11-13 16:53:52 +0100

    a

--- patatas.c/colaDeDatos.h
+++ patatas.c/colaDeDatos.h
@@ -1,1 +1,21 @@
-a
+#include <stdbool.h>
+
+typedef struct tipoElementoCola;
+typedef struct celdaC{
+	tipoElementoCola elem;
+	struct celdaC* sig;
+} celdaCola; 
+typedef struct tipoC{
+	celdaCola* ini;
+	celdaCola* fin;
+}tipoCola;
+
+void nuevaCola(tipoCola*);
+
+bool esNulaCola(tipoCola);
+
+void encolar(tipoCola*, tipoElementoCola);
+
+void desencolar(tipoCola*);
+
+tipoElementoCola frente(tipoCola);
