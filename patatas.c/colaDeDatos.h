940c48e1ad57754f407d3fad3685ce61949b504b
FerminEzcurdia <149517760+FerminEzcurdia@users.noreply.github.com>
2023-11-14 18:07:03 +0100

    Update colaDeDatos.h

--- patatas.c/colaDeDatos.h
+++ patatas.c/colaDeDatos.h
@@ -1,8 +1,29 @@
 #include <stdbool.h>
 
 typedef struct tipoElementoC{
-
+	int anxiety_level;
+	int self_esteem;
+	int mental_health_history;
+	int depression;
+	int headache;
+	int blood_pressure;
+	int sleep_quality;
+	int breathing_problem;
+	int noise_level;
+	int living_conditions;
+	int safety;
+	int basic_needs;
+	int academic_performance;
+	int study_load;
+	int teacher_student_relationship;
+	int future_career_concerns;
+	int social_support;
+	int peer_pressure;
+	int extracurricular_activities;
+	int bullying;
+	int stress_level;
 }tipoElementoCola;
+
 typedef struct celdaC{
 	tipoElementoCola elem;
 	struct celdaC* sig;
