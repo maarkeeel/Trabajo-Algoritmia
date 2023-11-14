d47da8bf7da6ed605eddd8cb43278b9b96a1ff85
FerminEzcurdia <149517760+FerminEzcurdia@users.noreply.github.com>
2023-11-14 18:40:58 +0100

    Update Trabajo.c

--- patatas.c/Trabajo.c
+++ patatas.c/Trabajo.c
@@ -3,3 +3,25 @@
 #include <stdbool.h>
 
 #include "colaDeDatos.h"
+
+int main(){
+  int multiplicador, guarda, numero;
+  FILE *StressLevelDataset;
+  
+  while(!feof(f))
+  {
+    fread(&multiplicador,sizeof(int),1,f);
+    fread(&guarda,sizeof(int),1,f);
+    if (guarda != ';')
+    {
+      multiplicador = multiplicador*10;
+      multiplicador += guarda;
+      fread(&guarda, sizeof(int),1,f);
+    }
+  }
+   			if(n>0){
+   				printf("%c", x);
+   			}
+   }
+  
+}
