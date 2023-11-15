0357ce3c11801fabea4142fd062b396999d2e39b
AimerDelCarmen <150698895+AimerDelCarmen@users.noreply.github.com>
2023-11-15 18:42:50 +0100

    Add files via upload
    Para cuando queramos meter un nuevo dato por teclado

---
+++ patatas.c/NuevoDato.c
@@ -0,0 +1,46 @@
+tipoElementoC NuevoDato()
+{
+    tipoElementoC nuevoStruct;
+    printf("anxiety_level"));
+    scanf("%d", &nuevoStruct.anxiety_level);
+    printf("self_esteem");
+    scanf("%d", &nuevoStruct.self_esteem);
+    printf("mental_health_history");
+    scanf("%d", &nuevoStruct.mental_health_history);
+    printf("depresion");
+    scanf("%d", &nuevoStruct.depresion);
+    printf("headache");
+    scanf("%d", &nuevoStruct.headache);
+    printf("blood_pressure");
+    scanf("%d", &nuevoStruct.blood_pressure);
+    printf("sleep_quality");
+    scanf("%d", &nuevoStruct.sleep_quality);
+    printf("breathing_problem");
+    scanf("%d", &nuevoStruct.breathing_problem);
+    printf("noise_level");
+    scanf("%d", &nuevoStruct.noise_level);
+    printf("living_conditions");
+    scanf("%d", &nuevoStruct.living_conditions);
+    printf("safety");
+    scanf("%d", &nuevoStruct.safety);
+    printf("basic_needs");
+    scanf("%d", &nuevoStruct.basic_needs);
+    printf("academic_performance");
+    scanf("%d", &nuevoStruct.academic_performance);
+    printf("study_load");
+    scanf("%d", &nuevoStruct.study_load);
+    printf("teacher_student_relationship");
+    scanf("%d", &nuevoStruct.teacher_student_relationship);
+    printf("future_career_concerns");
+    scanf("%d", &nuevoStruct.future_career_concerns);
+    printf("social_support");
+    scanf("%d", &nuevoStruct.social_support);
+    printf("peer_pressure");
+    scanf("%d", &nuevoStruct.peer_pressure);
+    printf("extracurricular_activities");
+    scanf("%d", &nuevoStruct.extracurricular_activities);
+    printf("bullying");
+    scanf("%d", &nuevoStruct.bullying);
+    nuevoStruct.stress_level = -1;
+    return nuevoStruct;
+}
