8244d409a1d000c051ab0182c967306bd3daf797
maarkeeel <149517688+maarkeeel@users.noreply.github.com>
2023-11-19 19:35:47 +0100

    Update Trabajo.c

--- patatas.c/Trabajo.c
+++ patatas.c/Trabajo.c
@@ -8,7 +8,7 @@ void Normalizar(*TipoElementoC)
 
 int main()
 {
-  int n1, n2, numero, indice;
+  int n1, n2, indice;
   FILE *StressLevelDataset;
 
   tipoElementoCola elem;
@@ -43,26 +43,26 @@ int main()
       }
     }
 
-    *datos.anxiety_level = 
-    *datos.self_esteem = 
-    *datos.mental_health_history = 
-    *datos.depression = 
-    *datos.headache = 
-    *datos.blood_pressure = 
-    *datos.sleep_quality = 
-    *datos.breathing_problem = 
-    *datos.noise_level = 
-    *datos.living_conditions = 
-    *datos.safety = 
-    *datos.basic_needs = 
-    *datos.academic_preformance = 
-    *datos.study_load = 
-    *datos.teacher_student_relationship = 
-    *datos.future_career_concerns = 
-    *datos.social_support = 
-    *datos.peer_pressure = 
-    *datos.extracurricular_activities = 
-    *datos.bullying = 
+    *datos.anxiety_level = tabla[0];
+    *datos.self_esteem = tabla[1];
+    *datos.mental_health_history = tabla[2];
+    *datos.depression = tabla[3];
+    *datos.headache = tabla[4];
+    *datos.blood_pressure = tabla[5];
+    *datos.sleep_quality = tabla[6];
+    *datos.breathing_problem = tabla[7];
+    *datos.noise_level = tabla[8];
+    *datos.living_conditions = tabla[9];
+    *datos.safety = tabla[10];
+    *datos.basic_needs = tabla[11];
+    *datos.academic_preformance = tabla[12];
+    *datos.study_load = tabla[13];
+    *datos.teacher_student_relationship = tabla[14];
+    *datos.future_career_concerns = tabla[15];
+    *datos.social_support = tabla[16];
+    *datos.peer_pressure = tabla[17];
+    *datos.extracurricular_activities = tabla[18];
+    *datos.bullying = tabla[19];
   }
 }
   
